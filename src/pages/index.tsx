18bef539e60423b98538c9e501debf9adfde8824
AgonIdrizi <agon.88@hotmail.com>
2020-12-16 15:50:46 +0100

    fix navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,14 @@ export default function Home() {
 
   return (
     <div className={`${theme} bg-primary  home-container`}>
-      <div className="navbar flex justify-around">
+      <div className="navbar top-0 absolute w-full flex justify-around">
         <NavLinks aboutDivRef={aboutDivRef} projectsDivRef={projectsDivRef} contanctDivRef={contanctDivRef} />
         <Toggle setTheme={setTheme} />
       </div>
       <div className="content flex-col justify-center">
-        <section ref={aboutDivRef} className="about-section flex flex-col my-20 justify-center items-center">
-          <h2 className="text-primary font-bold text-2xl my-2 justify-center">Ktu Logoja</h2>
-          <div className="text-primary">
+        <section ref={aboutDivRef} className="about-section flex flex-col mx-5 my-20 justify-center place-items-start">
+          <h2 className="text-primary font-bold text-2xl my-2 justify-start">Ktu Logoja</h2>
+          <div className="text-primary sm:max-w-2xl">
             <p className=" break-all">
               aasdasdasdasdaasdsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf
             </p>
@@ -38,24 +38,24 @@ export default function Home() {
             </p>
           </div>
         </section>
-        <section ref={projectsDivRef} className="projects-section flex flex-col w-500 m-16 justify-center items-center">
+        <section ref={projectsDivRef} className="projects-section flex flex-col w-500 mx-5 my-16 justify-center place-items-start">
           <h2 className="text-primary text-2xl font-bold justify-center">Projects</h2>
           <div className="projects-content">
             <div className="project-card h-32 w-64 my-10 border-gray-200 shadow-md">
               sdfdf
             </div>
-            <div className="project-card h-32 w-64 border-gray-200 shadow-md">
+            <div className="project-card h-32 w-64 my-10 border-gray-200 shadow-md">
               sdfdf
             </div>
-            <div className="project-card h-32 w-64 border-gray-200 shadow-md">
+            <div className="project-card h-32 w-64 my-10 border-gray-200 shadow-md">
               sdfdf
             </div>
-            <div className="project-card h-32 w-64 border-gray-200 shadow-md">
+            <div className="project-card h-32 w-64 my-10 border-gray-200 shadow-md">
               sdfdf
             </div>
           </div>
         </section>
-        <section ref={contanctDivRef} className="projects-section flex flex-col w-500 m-16 justify-center items-center">
+        <section ref={contanctDivRef} className="projects-section flex flex-col w-500  mx-5 my-16 justify-center place-items-start">
           <h2 className="text-primary text-2xl font-bold justify-center">Contact</h2>
           <div className="contacts-content">
             Reach me through my email: agon@agon.com
