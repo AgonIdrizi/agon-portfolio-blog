5bec79590274b3f43bce5525126cfc70753c44c3
AgonIdrizi <agon.88@hotmail.com>
2020-12-21 12:49:20 +0100

    add scrollToVisible hook

---
+++ src/hooks/scrollToVisibleHook.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { getDimensions } from "../utils/helpers";
+
+export const useScrollToVisible = (headerRef, sectionRefs = []) => {
+  const [visibleSection, setVisibleSection] = useState();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { height: headerHeight } = getDimensions(headerRef.current);
+      const scrollPosition = window.scrollY + headerHeight;
+
+      const selected: any = sectionRefs.find(({ section, ref }) => {
+        const ele = ref.current;
+        if (ele) {
+          const { offsetBottom, offsetTop } = getDimensions(ele);
+          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
+        }
+      });
+
+      if (selected && selected.section !== visibleSection) {
+        setVisibleSection(selected.section);
+      } else if (!selected && visibleSection) {
+        setVisibleSection(undefined);
+      }
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [visibleSection]);
+
+  return [visibleSection];
+};
--- src/pages/Components/NavLinks/NavLinks.tsx
+++ src/pages/Components/NavLinks/NavLinks.tsx
@@ -1,70 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import BreakLine from "../UI/BreakLine/BreakLine";
 import NavLink from "./NavLink";
-import { getDimensions, scrollTo } from '../../../utils/helpers';
+import { getDimensions, scrollTo } from "../../../utils/helpers";
+import { useScrollToVisible } from "../../../hooks/scrollToVisibleHook";
 
-
-const NavLinks = ({ headerRef, aboutDivRef, projectsDivRef, contanctDivRef }) => {
-  const [visibleSection, setVisibleSection] = useState();
-  const [contentInView, setContentInView] = useState("about");
+const NavLinks = ({
+  headerRef,
+  aboutDivRef,
+  projectsDivRef,
+  contanctDivRef,
+}) => {
   const sectionRefs = [
     { section: "About", ref: aboutDivRef },
     { section: "Projects", ref: projectsDivRef },
-    { section: "Contact", ref: contanctDivRef }
+    { section: "Contact", ref: contanctDivRef },
   ];
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { height: headerHeight } = getDimensions(headerRef.current);
-      const scrollPosition = window.scrollY + headerHeight;
-
-      const selected: any = sectionRefs.find(({section, ref}) => {
-        const ele = ref.current;
-        if (ele) {
-          const { offsetBottom, offsetTop } = getDimensions(ele);
-          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
-        }
-      });
-
-      if (selected && selected.section !== visibleSection) {
-        setVisibleSection(selected.section);
-      } else if (!selected && visibleSection) {
-        setVisibleSection(undefined)
-      }
-    };
-    handleScroll();
-    window.addEventListener("scroll", handleScroll)
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [visibleSection])
-
+  const [visibleSection] = useScrollToVisible(headerRef, sectionRefs);
 
   return (
     <div className="navlinks flex">
-      <div>
-        <NavLink
-          title="About"
-          onNavLinkClick={() => scrollTo(aboutDivRef.current)}
-          contentInView={visibleSection}
-        />
-        {visibleSection === "About" && <BreakLine />}
-      </div>
-      <div>
-        <NavLink
-          title="Projects"
-          onNavLinkClick={() => scrollTo(projectsDivRef.current)}
-          contentInView={visibleSection}
-        />
-        {visibleSection === "Projects" && <BreakLine />}
-      </div>
-      <div>
-        <NavLink
-          title="Contact"
-          onNavLinkClick={() => scrollTo(contanctDivRef.current)}
-          contentInView={visibleSection}
-        />
-        {visibleSection === "Contact" && <BreakLine />}
-      </div>
+      {sectionRefs.map(({ section, ref }) => (
+        <div>
+          <NavLink
+            title={section}
+            onNavLinkClick={() => scrollTo(ref.current)}
+            contentInView={visibleSection}
+          />
+        </div>
+      ))}
     </div>
   );
 };
