801e094bb95a5873276be86aca9b85f18ca46efe
AgonIdrizi <agon.88@hotmail.com>
2021-02-04 15:45:32 +0100

    add ProjectCard component

---
+++ src/pages/Components/ProjectCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectCard = ({title, description, href, icon}) => {
+  return (
+    <div className="flex items-center border border-gray-200 dark:border-gray-500 rounded mb-4 p-4">
+        <div>{icon}</div>
+        <div>
+          <h4 className="text-lg font-bold tracking-tight text-primary dark:text-gray-100">{title}</h4>
+          <p className="leading-5 text-gray-700 dark:text-gray-300">{description}</p>
+        </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import NavLinks from './Components/NavLinks/NavLinks'
 import { withRouter } from "next/dist/client/router";
 import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
 import Container from "./Components/Container";
+import ProjectCard from "./Components/ProjectCard";
 
 
 export default function Home() {
@@ -24,26 +25,26 @@ export default function Home() {
         <section ref={aboutDivRef} className="about-section flex flex-col mx-5  justify-center place-items-start">
           <h2 className="text-primary font-bold text-3xl md:text-5xl my-2 justify-start">Hi, I'm Agon Idrizi</h2>
           <div className="text-primary sm:max-w-2xl">
-            <h2 className="text-gray-600 dark:text-gray-200  break-all">
+            <h2 className="text-gray-600 leading-6 dark:text-gray-200  break-all">
               I'm a developer, tech entusiast, writer. My passion is building web applications from ground to mature products. There is more info in about section!
             </h2>
           </div>
         </section>
         <section ref={projectsDivRef} className="projects-section flex flex-col w-500 mx-5 my-16 pt-4 sm:pt-8  justify-center place-items-start">
-          <h2 className="text-primary text-2xl md:text-4xl font-bold justify-center">Projects</h2>
+          <h2 className="text-primary text-2xl md:text-4xl mb-6 font-bold justify-center">Projects</h2>
           <div className="projects-content ">
-            <div className={`project-card text-primary h-32 w-64 my-10 dark:text-gray-200 rounded-md  border-gray-200 shadow-md`}>
-              sdfdf
-            </div>
-            <div className={`project-card text-primary h-32 w-64 my-10 dark:text-gray-200 rounded-md  rounded-md  border-gray-200 shadow-md`}>
-              sdfdf
-            </div>
-            <div className={`project-card text-primary h-32 w-64 my-10 dark:text-gray-200 rounded-md rounded-md  border-gray-200 shadow-md`}>
-              sdfdf
-            </div>
-            
+            <ProjectCard 
+              title="My Dinner" 
+              description="Online food delivery service for local fast foods built with React.js" 
+              href="" 
+              icon="" />
+            <ProjectCard title="Where's Waldo" description="" href="" icon="" />
+            <ProjectCard title="Fast replies" description="" href="" icon="" />
           </div>
         </section>
+        <section className="projects-section flex flex-col w-500 mx-5  pt-4 sm:pt-8  justify-center place-items-start">
+          <h2 className="text-primary text-2xl md:text-4xl mb-6 font-bold justify-center">Blogs</h2>
+        </section >
         <section ref={contanctDivRef} className="projects-section flex flex-col w-500  mx-5 mt-16 pt-4 sm:pt-8  justify-center place-items-start">
           <h2 className="text-primary text-2xl font-bold justify-center">Contact</h2>
           <div className="contacts-content text-primary">
