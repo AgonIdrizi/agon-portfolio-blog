5ee6851c9ab0d3b3605353f785aa81afba2bebf4
AgonIdrizi <agon.88@hotmail.com>
2021-02-08 14:24:57 +0100

    fix Project Card css

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 
-const ProjectCard = ({title, description, href, icon}) => {
+const ProjectCard = ({title, description, slug, icon}) => {
   return (
-    <Link href={href} passHref>
-      <a >
-    <div className="flex items-center border border-gray-200 dark:border-red-100 rounded mb-4 p-4">
-        <div>{icon}</div>
-        <div>
-          <h4 className="text-lg font-bold tracking-tight text-primary dark:text-gray-100">{title}</h4>
-          <p className="leading-5 text-sm md:text-base lg:text-base text-gray-700 dark:text-gray-300">{description}</p>
+    <Link href={`/projects/${slug}`} >
+      <a className="w-full">
+        <div className="flex items-center border border-gray-200 dark:border-red-100 rounded mb-4 p-4">
+          <div>{icon}</div>
+          <div>
+            <h4 className="text-lg font-bold tracking-tight text-primary hover:text-red-200 dark:text-gray-100">{title}</h4>
+            <p className="leading-5 text-sm md:text-base lg:text-base text-gray-700 dark:text-gray-300">{description}</p>
+          </div>
         </div>
-    </div>
-    </a>
+      </a>
     </Link>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Head from "next/head";
-import { useRef, useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useRef} from "react";
 
-
-import { withRouter } from "next/dist/client/router";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import Container from "../components/Container";
 import ProjectCard from "../components/ProjectCard";
@@ -24,10 +20,10 @@ export default function Home() {
       
       <div className="flex flex-col items-start max-w-2xl mx-auto mb-16  justify-center">
         <section ref={aboutDivRef} className="about-section flex flex-col mx-5  justify-center place-items-start">
-          <h2 className="text-primary font-bold text-3xl md:text-5xl my-2 justify-start">Hi, I'm Agon Idrizi</h2>
+          <h2 className="text-primary font-bold text-3xl md:text-5xl my-2 justify-start">Hi, I'm Agon!</h2>
           <div className="text-primary  sm:max-w-2xl">
-            <h2 className="text-gray-600 leading-6 text-sm md:text-base lg:text-base  dark:text-gray-200  break-all">
-              I'm a developer, tech entusiast, writer. My passion is building web applications from ground to mature products. There is more info in about section!
+            <h2 className="text-gray-600 leading-6 text-sm md:text-base lg:text-lg  dark:text-gray-200  break-all">
+              I'm a front end developer, building websites with React and Typescript. Sometimes i write blogs and capture beauty of the world with my fuji camera!
             </h2>
           </div>
         </section>
@@ -37,10 +33,10 @@ export default function Home() {
             <ProjectCard 
               title="My Dinner" 
               description="Online food delivery service for local fast foods built with React.js" 
-              href="/projects/agon-test" 
+              slug="agon-test" 
               icon="" />
-            <ProjectCard title="Where's Waldo" description="" href="" icon="" />
-            <ProjectCard title="Fast replies" description="" href="" icon="" />
+            <ProjectCard title="Where's Waldo" description="" slug="" icon="" />
+            <ProjectCard title="Fast replies" description="" slug="" icon="" />
           </div>
         </section>
         <section className="projects-section flex flex-col w-500 mx-5  pt-4 sm:pt-8  justify-center place-items-start">
