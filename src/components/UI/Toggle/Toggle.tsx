8e8abc5cadb9dbf051b818d1e6e2043041dee457
AgonIdrizi <agon.88@hotmail.com>
2021-02-15 17:55:51 +0100

    fix css

--- src/components/UI/Toggle/Toggle.tsx
+++ src/components/UI/Toggle/Toggle.tsx
@@ -1,5 +1,4 @@
 import { useTheme } from 'next-themes';
-import styles from './styles.module.css'
 
 export default function Toggle () {
   const { theme, setTheme } = useTheme();
@@ -14,8 +13,8 @@ export default function Toggle () {
             onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
             className="hidden"
           />
-          <div className={`${styles.toggle__line} w-10 h-4 bg-gray-400 rounded-full shadow-inner`}></div>
-          <div className={`${styles.toggle__dot} absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0`}></div>
+          <div className={`toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner`}></div>
+          <div className={`toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0`}></div>
         </div>
       </label>
     </div>
