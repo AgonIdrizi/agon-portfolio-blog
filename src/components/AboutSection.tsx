7f22793faab6ec74da6fb168e7d8f2ec0311ca66
AgonIdrizi <agon.88@hotmail.com>
2021-02-17 13:01:51 +0100

    css fix

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,9 +5,9 @@ const AboutSection = () => {
     <>
       <h2 className="text-primary dark:text-gray-200 text-2xl mb-4 font-bold justify-center">About</h2>
       <div className="contacts-content text-sm md:text-base lg:text-base text-primary dark:text-gray-200">
-        <p className="text-sm md:text-base lg:text-base text-gray-600 dark:text-gray-400">Hey, I'm Agon. I'm a developer. I create modern full-stack web applications with expertise in React/Typescript.</p>
-        <p className="text-sm md:text-base mt-2 lg:text-base text-gray-600 dark:text-gray-400">I have an innate desire to create - websites, programs, pictures, music - you name it. My passion lies in the intersection of creation and communication. I want to create tools to help humans solve problems faster!</p>
-        <p className="text-sm md:text-base mt-2 lg:text-base text-gray-600 dark:text-gray-400">For contact reach me through: agonidrizi88@gmail.com</p>
+        <p className="text-sm leading-6 md:text-base lg:text-base text-gray-600 dark:text-gray-200">Hey, I'm Agon. I'm a developer. I create modern full-stack web applications with expertise in React/Typescript.</p>
+        <p className="text-sm leading-6 md:text-base mt-2 lg:text-base text-gray-600 dark:text-gray-200">I have an innate desire to create - websites, programs, pictures, music - you name it. My passion lies in the intersection of creation and communication. I want to create tools to help humans solve problems faster!</p>
+        <p className="text-sm leading-6 md:text-base mt-2 lg:text-base text-gray-600 dark:text-gray-200">For contact reach me through: agonidrizi88@gmail.com</p>
       </div>
     </>
   );
